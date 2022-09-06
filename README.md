1f7f94b187f492b95a64baa43c9eee5893af0808
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-09-06 20:09:50 +0530

    readme additioin and env updates

---
+++ README.md
@@ -0,0 +1,13 @@
+# CRUD Application React & Supabase
+
+**Frontend:** React, TailwindCSS
+
+**Backend:** SupaBase
+
+**Database:** Postgres
+
+**Note:** Use "VITE_ENV_NAME" for Env variables instead of "REACT_APP_ENV_NAME".
+
+## Screenshots
+
+![UI](https://github.com/SujoyKrHaldar/Supabase-React-CRUD/blob/main/public/layout.png)
--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,11 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = "https://zohphnqqipwjrwdqrbem.supabase.co";
-// const supabaseKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvaHBobnFxaXB3anJ3ZHFyYmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzNzA2MDYsImV4cCI6MTk3Nzk0NjYwNn0.ZyTI5-LkirJbT4q0a20u16MIVeRBbqWcb-GEoCKcjVk";
-
-const supabaseKey = import.meta.env.REACT_APP_SUPABASE_KEY;
-const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
